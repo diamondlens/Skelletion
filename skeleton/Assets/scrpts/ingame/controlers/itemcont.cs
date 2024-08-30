fe3365386a966379b2ef3bce47a400e272e75c86
diamondlens <166950247+diamondlens@users.noreply.github.com>
2024-08-30 14:34:45 -0700

    item finish

--- skeleton/Assets/scrpts/ingame/controlers/itemcont.cs
+++ skeleton/Assets/scrpts/ingame/controlers/itemcont.cs
@@ -33,13 +33,19 @@ public class itemcont : MonoBehaviour
     GameObject twopick;
     GameObject threepick;
 
+    public GameObject nochoice1;
+    public GameObject nochoice2;
+    public GameObject nochoice3;
+
     public GameObject boneshards;
+    public GameObject hellsdecent;
     // Start is called before the first frame update
     void Start()
     {
 
 
         rpool.Add(boneshards);
+        rpool.Add(hellsdecent);
 
         cmax = cpool.Count;
         rmax = rpool.Count;
@@ -64,39 +70,50 @@ public class itemcont : MonoBehaviour
 
     void upgradepick1()
     {
-
+        if (av <= 0)
+        {
+            onepick = nochoice1;
+            onepick.SetActive(true);
+            onepick.transform.localPosition = new Vector3(-200, 0, 0);
+            upgradepick2();
+        }
         if (av > 0)
         {
 
-            print("hi");
             raritypick = Random.Range(1, 20);
 
             if (raritypick <= 11 && cav > 0)
             {
                 //common (55%)
                 onepick = Cpick();
+                cav -= 1;
             }
             if (raritypick > 11 && raritypick <= 16 && rav > 0)
             {
                 //rare (30%)
+
                 onepick = Rpick();
+                rav -= 1;
             }
             if (raritypick > 16 && raritypick <= 19 && eav > 0)
             {
                 //epic (10%)
                 onepick = Epick();
+                eav -= 1;
             }
             if (raritypick > 19 && lav > 0)
             {
                 //legendary (5) 
                 onepick = Lpick();
+                lav -= 1;
             }
 
             if (onepick != null)
             {
-
                 onepick.SetActive(true);
                 onepick.transform.localPosition = new Vector3(-200, 0, 0);
+                av -= 1;
+                upgradepick2();
             }
 
             if (onepick == null)
@@ -104,91 +121,173 @@ public class itemcont : MonoBehaviour
                 upgradepick1();
             }
         }
-
     }
 
     void upgradepick2()
     {
-
-        if (av >= 0)
+        if (av <= 0)
+        {
+            twopick = nochoice2;
+            twopick.SetActive(true);
+            twopick.transform.localPosition = new Vector3(0, 0, 0);
+            upgradepick3();
+        }
+        if (av > 0)
         {
             raritypick = Random.Range(1, 20);
+
             if (raritypick <= 11)
             {
                 //common (55%)
-                twopick = Cpick();
+                if (cav > 0)
+                {
+                    twopick = Cpick();
+                }
+                cav -= 1;
             }
-            if (raritypick > 11 && raritypick <= 16)
+            if (raritypick > 11 && raritypick <= 16 && rav > 0)
             {
                 //rare (30%)
-                twopick = Rpick();
+                if (rav > 0)
+                {
+                    twopick = Rpick();
+                }
+                rav -= 1;
             }
-            if (raritypick > 16 && raritypick <= 19)
+            if (raritypick > 16 && raritypick <= 19 && eav > 0)
             {
                 //epic (10%)
-                twopick = Epick();
+                if (eav > 0)
+                {
+                    twopick = Epick();
+                }
+                eav -= 1;
             }
-            if (raritypick > 19)
+            if (raritypick > 19 && lav > 0)
             {
                 //legendary (5) 
-                twopick = Lpick();
+                if (lav > 0)
+                {
+                    twopick = Lpick();
+                }
+                lav -= 1;
             }
 
-            bool check2 = true;
-            //check2 = check(twopick);
-
-            if (check2 == true)
+            if (twopick != onepick)
             {
-                twopick.SetActive(true);
-                twopick.transform.localPosition = new Vector3(0, 0, 0);
-                upgradepick3();
+                if (twopick != null)
+                {
+                    twopick.SetActive(true);
+                    twopick.transform.localPosition = new Vector3(0, 0, 0);
+                    av -= 1;
+                    upgradepick3();
+                }
             }
-            if (check2 == false)
+
+            if (twopick == null || twopick == onepick)
             {
+                if (raritypick <= 11)
+                {
+                    cav += 1;
+                }
+                if (raritypick > 11 && raritypick <= 16)
+                {
+                    rav += 1;
+                }
+                if (raritypick > 16 && raritypick <= 19)
+                {
+                    eav += 1;
+                }
+                if (raritypick > 19)
+                {
+                    lav += 1;
+                }
                 upgradepick2();
+
             }
         }
     }
 
     void upgradepick3()
     {
+        if (av <= 0)
+        {
+            threepick = nochoice3;
+            threepick.SetActive(true);
+            threepick.transform.localPosition = new Vector3(200, 0, 0);
+        }
 
-        if (av >= 0)
+        if (av > 0)
         {
             raritypick = Random.Range(1, 20);
 
             if (raritypick <= 11)
             {
                 //common (55%)
-                threepick = Cpick();
+                if (cav > 0)
+                {
+                    threepick = Cpick();
+                }
+                cav -= 1;
             }
-            if (raritypick > 11 && raritypick <= 16)
+            if (raritypick > 11 && raritypick <= 16 && rav > 0)
             {
                 //rare (30%)
-                threepick = Rpick();
+                if (rav > 0)
+                {
+                    threepick = Rpick();
+                }
+                rav -= 1;
             }
-            if (raritypick > 16 && raritypick <= 19)
+            if (raritypick > 16 && raritypick <= 19 && eav > 0)
             {
                 //epic (10%)
-                threepick = Epick();
+                if (eav > 0)
+                {
+                    threepick = Epick();
+                }
+                eav -= 1;
             }
-            if (raritypick > 19)
+            if (raritypick > 19 && lav > 0)
             {
                 //legendary (5) 
-                threepick = Lpick();
+                if (lav > 0)
+                {
+                    threepick = Lpick();
+                }
+                lav -= 1;
             }
 
-            bool check3 = true;
-            //check3 = check(threepick);
-
-            if (check3 == true)
+            if (threepick != onepick && threepick != twopick)
             {
-                threepick.SetActive(true);
-                threepick.transform.localPosition = new Vector3(200, 0, 0);
+                if (threepick != null)
+                {
+                    threepick.SetActive(true);
+                    threepick.transform.localPosition = new Vector3(200, 0, 0);
+                    av -= 1;
+                }
             }
-            if (check3 == false)
+
+            if (threepick == null || threepick == onepick || threepick == twopick)
             {
+                if (raritypick <= 11)
+                {
+                    cav += 1;
+                }
+                if (raritypick > 11 && raritypick <= 16)
+                {
+                    rav += 1;
+                }
+                if (raritypick > 16 && raritypick <= 19)
+                {
+                    eav += 1;
+                }
+                if (raritypick > 19)
+                {
+                    lav += 1;
+                }
                 upgradepick3();
+
             }
         }
     }
@@ -211,4 +310,15 @@ public class itemcont : MonoBehaviour
         return cpool[Random.Range(0, lpool.Count)];
     }
 
+    private void Reset()
+    {
+        cmax = cpool.Count;
+        rmax = rpool.Count;
+        emax = epool.Count;
+        lmax = lpool.Count;
+
+        avmax = cmax + rmax + emax + lmax;
+        av = avmax;
+        rav = rmax;
+    }
 }
